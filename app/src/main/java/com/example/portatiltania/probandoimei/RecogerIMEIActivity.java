a266dd13ab7cf5a0e3331b211b940e693e9c7be2
tania77 <ainatrg@gmail.com>
2017-03-19 22:05:39 +0000

    Initial commit

---
+++ app/src/main/java/com/example/portatiltania/probandoimei/RecogerIMEIActivity.java
@@ -0,0 +1,28 @@
+package com.example.portatiltania.probandoimei;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class RecogerIMEIActivity extends AppCompatActivity {
+    private static final int REQUEST_READ_PHONE_STATE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recoger_imei);
+        TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+        }
+        Log.d("RecogerIMEI",telephonyManager.getDeviceId());
+    }
+}
